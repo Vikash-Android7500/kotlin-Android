d149a66f6d784edb3b45d9b486ec87df3feebb7c
vikas <vikash75@gmail.com>
2021-04-13 23:27:22 +0530

    Tutorial Kotlin Fist App

---
+++ app/src/main/java/com/vk_in/dice2/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.vk_in.dice2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vk_in.dice2.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        binding.caunter.setOnClickListener { calculateTip() }
+
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.amaunt.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            binding.result.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.r1 -> 0.15
+            R.id.r2 -> 0.18
+            else -> 0.20
+        }
+
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.result.text = getString(R.string.tip_amount, formattedTip)
+    }
+}
+
+
---
+++ app/src/main/java/com/vk_in/dice2/SplashActivity.kt
@@ -0,0 +1,55 @@
+package com.vk_in.dice2
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import androidx.appcompat.app.AppCompatActivity
+import com.vk_in.dice2.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }
+        else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        val splashAnimation = android.view.animation.AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashBinding.splash1.animation = splashAnimation
+
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener{
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+              Handler(Looper.getMainLooper()).postDelayed({
+                  startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                  finish()
+              }, 1000)//One second
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+}
