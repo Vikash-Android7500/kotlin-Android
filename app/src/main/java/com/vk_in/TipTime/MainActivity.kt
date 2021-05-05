4783e3fb40a598bbf05cdbab1253aa940645e5bd
vikas <vikash75@gmail.com>
2021-05-05 22:02:47 +0530

    Dark Theme system setting update2

--- app/src/main/java/com/vk_in/TipTime/MainActivity.kt
+++ app/src/main/java/com/vk_in/TipTime/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.vk_in.TipTime
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //id set
+        val offlite = findViewById<ImageView>(R.id.offlite)
+        val onlite = findViewById<ImageView>(R.id.onlite)
+        val toggle: ToggleButton = findViewById(R.id.toggleButton)
+        //setOnclicklener
+
+        toggle.setOnCheckedChangeListener { _:CompoundButton?, isChecked ->
+           if (isChecked){
+               onlite.setBackgroundColor(0xff00ff00.toInt())
+           }
+            else{
+                offlite.setBackgroundColor(0xffff0000.toInt())
+           }
+
+
+
+        }
+
+    }
+}
+
+private fun ImageView.setBackground(on: Int) {
+
+    val isChecked = false
+    if (isChecked) {
+        val offlite = null
+        offlite?.setBackground(R.drawable.off)
+    }
+
+    else {
+        val onlite = null
+        onlite?.setBackground(R.drawable.on)
     }
 }
